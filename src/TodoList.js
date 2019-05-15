6a78c13287a37f3c199c28a07a17c74d537bb50e
Sergio Rosa <scarlosjazz@gmail.com>
2019-05-14 21:50:55 -0400

    Manipulating Todo List

--- src/TodoList.js
+++ src/TodoList.js
@@ -1,14 +1,18 @@
 import React from "react";
 
-import { View, Text } from "react-native";
+import { View, Text, Button } from "react-native";
 import { connect } from "react-redux";
 
 // import { Container } from './styles';
-const TodoList = ({ todos }) => (
+const TodoList = ({ todos, dispatch }) => (
   <View style={{ flex: 1, backgroundColor: "#FFF", justifyContent: "center" }}>
     {todos.map(todo => (
       <Text key={todo}>{todo}</Text>
     ))}
+    <Button
+      onPress={() => dispatch({ type: "ADD_TODO", text: "New todo" })}
+      title="Adding todo"
+    />
   </View>
 );
 
--- src/store/index.js
+++ src/store/index.js
@@ -1,8 +1,21 @@
 import { createStore } from "redux";
 
 //Reducer
-function reducer() {
-  return ["Play Piano", "Learn Harmony"];
+const INITIAL_STATE = ["Play Piano", "Learn Harmony"];
+
+//Actions
+
+// Add todo
+//Mark todo as complete
+
+// { type: 'ADD_TODO', text: 'New todo' }
+// { type: 'MARK_AS_COMPLETED', id: 4 }
+
+function reducer(state = INITIAL_STATE, action) {
+  if (action.type === "ADD_TODO") {
+    return [...state, action.text];
+  }
+  return state;
 }
 
 const store = createStore(reducer);
